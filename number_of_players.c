1b0a9d149b4f23dd6eb257ec1c2add738ac175cc
Luiz Gustavo Monteiro <luizgu_mreis@hotmail.com>
2025-03-11 00:29:06 -0300

    Define Number of Players

---
+++ number_of_players.c
@@ -0,0 +1,29 @@
+#include "number_of_players.h"
+#include <stdint.h>
+
+int define_number_of_players(int number_players)
+{
+    number_players = 0;
+
+    do
+    {
+        printf("Players: ");
+
+        if (scanf("%d", number_players != 1))
+        {
+            printf("Invalid Value!\n");
+            while (getchar() != '\n')
+                ;
+            continue;
+        }
+
+        if (number_players == 0 || number_players > MAX_PLAYERS)
+        {
+            printf("Number invalid! Type a valid value!");
+        }
+    } while (number_players == 0 || number_players > MAX_PLAYERS);
+
+    printf("Number of players: %d\n", number_players);
+
+    return number_players;
+}
---
+++ number_of_players.h
@@ -0,0 +1,7 @@
+#ifndef NUMBER_OF_PLAYERS_H
+#define NUMBER_OF_PLAYERS_H
+#define MAX_PLAYERS 4
+
+int define_number_of_players(int number_players);
+
+#endif
