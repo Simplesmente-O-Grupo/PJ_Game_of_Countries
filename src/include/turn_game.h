d7d86551b8f8cb8b3d3a5c437c1dfdbc9e8025fa
Luiz Gustavo Monteiro <luizgu_mreis@hotmail.com>
2025-03-22 15:14:41 -0300

    RF08-12-TurnGame-Bug

--- src/include/turn_game.h
+++ src/include/turn_game.h
@@ -1,9 +1,12 @@
 #ifndef TURN_GAME_H
 #define TURN_GAME_H
 
+void displayHelpMenu();
 void turnGame();
-void tieTurn();
-void winnerTurn();
-void gameEnd();
+PlayerNode *handleBattle(PlayerNode *currentPlayer, PlayerNode *playerList, int chooseAttribute, Deck *battleDeck);
+PlayerNode *handleWar(PlayerNode *playerList, int chooseAttribute, Deck *battleDeck);
+void eliminatePlayer();
+void checkGameOver();
+int countPlayersWithCards();
 
 #endif
