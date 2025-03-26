a39185e662c7eabf47aa9e94b1ff0f04fc40d4ce
Luiz Gustavo Monteiro <luizgu_mreis@hotmail.com>
2025-03-26 19:56:58 -0300

    Comment card.c

--- src/card.c
+++ src/card.c
@@ -5,14 +5,14 @@
 
 // FUNCTIONS IMPLEMENTATIONS
 
-Card createCard(int id, char *name, char *category, int defence, int army, int navy, int airforce) {
+Card createCard(int id, char *name, char *category, int defence, int army, int navy, int airforce)
+{
 	Card card = {
 		.id = id,
 		.defence = defence,
 		.army = army,
 		.navy = navy,
-		.airforce = airforce
-	};
+		.airforce = airforce};
 
 	strcpy(card.name, name);
 	strcpy(card.category, category);
@@ -20,26 +20,27 @@ Card createCard(int id, char *name, char *category, int defence, int army, int n
 	return card;
 }
 
-int cardGetAttribute(Card card, CardAttribute attr) {
+// Pega o atributo da carta e retornar o seu valor
+int cardGetAttribute(Card card, CardAttribute attr)
+{
 	int attrValue;
 	switch (attr)
 	{
-		case AIRFORCE:
-			attrValue = card.airforce;
-			break;
-		case NAVY:
-			attrValue = card.navy;
-			break;
-		case DEFENCE:
-			attrValue = card.defence;
-			break;
-		case ARMY:
-			attrValue = card.army;
-			break;
-		default:
-			attrValue = card.army;
-			break;
+	case AIRFORCE:
+		attrValue = card.airforce;
+		break;
+	case NAVY:
+		attrValue = card.navy;
+		break;
+	case DEFENCE:
+		attrValue = card.defence;
+		break;
+	case ARMY:
+		attrValue = card.army;
+		break;
+	default:
+		attrValue = card.army;
+		break;
 	}
 	return attrValue;
 }
-
